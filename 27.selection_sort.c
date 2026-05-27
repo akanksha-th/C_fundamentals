a2b6167e4b877be00782896dd88b78d34ec68df3
Akanksha <ak.t.kree@gmail.com>
2026-05-27 07:34:20 +0530

    add selection sort implementation

--- 27.selection_sort.c
+++ 27.selection_sort.c
@@ -1,7 +1,53 @@
-// gcc .\26.bubble_sort.c -o out.exe; if ($?) { .\out.exe }
+// gcc .\27.selection_sort.c -o out.exe; if ($?) { .\out.exe }
 
 /*
-Selection Sort: 
+Selection Sort: it sorts by repeatedly selecting the smallest (or largest) element from the unsorted portion of the array and swapping it with the first unsorted element.
+    1. Start with the first element as the minimum.
+    2. Compare the minimum with the next element. If the next element is smaller, update the minimum.
+    3. Continue this process for the entire array. After the first pass, the minimum element is placed at the beginning of the array.
+    4. Repeat the process for the remaining unsorted portion of the array until the entire array is sorted.
 
-Write a program that implements selection sort. After it starts working, generalise bubble sort to accept a comparator.
+Write a program that implements selection sort. 
 */
+
+# include <stdio.h>
+
+void selection_sort(int *arr, int n){
+    for (int i=0; i<n-1; i++){
+        int min = i;
+        int swapped = 0;
+        for (int j=i+1; j<n; j++){
+            if (arr[j] < arr[min]){ 
+                min = j; // track the index of the minimum value, don't swap yet
+            }
+        }
+        if (min !=i){ // // swap once after finding true minimum
+            int temp = arr[min];
+            arr[min] = arr[i];
+            arr[i] = temp;
+            swapped = 1;
+        }
+        if (!swapped) break;
+    }
+};
+
+void print_array(int *arr, int n){
+    for (int i=0; i<n; i++){
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+};
+
+int main(){
+    int arr[] = {64, 25, 12, 22, 11};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    printf("Original array: \n");
+    print_array(arr, n);
+
+    selection_sort(arr, n);
+    printf("Sorted array: \n");
+    print_array(arr, n);
+
+    return 0;
+}
